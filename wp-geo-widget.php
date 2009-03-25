c71910fa4979facedbf073932f1d5cfc8fb98dce
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-25 21:27:24 +0000

    Update widget and language POT file.
    git-svn-id: https://plugins.svn.wordpress.org/wp-geo/trunk@105173 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-geo-widget.php
+++ wp-geo-widget.php
@@ -127,7 +127,7 @@ class WPGeoWidget
 		if (!$wpgeo->checkGoogleAPIKey())
 		{
 			// NOTE: Check if there is a 'less hard-coded' way to write link to settings page
-			echo '<p class="wp_geo_error">WP Geo is not currently active as you have not entered a Google API Key. Please <a href="' . get_bloginfo('wpurl') . '/wp-admin/options-general.php?page=wp-geo/wp-geo.php">update your WP Geo settings</a>.</p>';
+			echo '<p class="wp_geo_error">' . __('WP Geo is not currently active as you have not entered a Google API Key', 'wp-geo') . '. <a href="' . get_bloginfo('wpurl') . '/wp-admin/options-general.php?page=wp-geo/wp-geo.php">' . __('Please update your WP Geo settings', 'wp-geo') . '</a>.</p>';
 		}
 		echo '
 			<p><label for="wpgeo-title">' . __('Title', 'wp-geo') . ': <input class="widefat" id="wpgeo-title" name="wpgeo-title" type="text" value="' . $title . '" /></label></p>
