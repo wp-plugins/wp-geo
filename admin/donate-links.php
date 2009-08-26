ea77f8b4fe6eb491d4d99d2f1ff147255dee3733
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-26 20:17:47 +0000

    Tag 3.0.9.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-geo/tags/3.0.9.2@149395 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/donate-links.php
+++ admin/donate-links.php
@@ -15,7 +15,7 @@
 <ul class="subsubsub donate-links" style="float:none;">
 	<li>Support this plugin: </li>
 	<li><a href="http://www.wpgeo.com/donate" target="_blank">Make a Donation</a> |</li>
-	<li><a href="http://www.amazon.co.uk/registry/wishlist/3GOX0MMJTRD3C" target="_blank">Buy me something</a> |</li>
+	<li><a href="http://www.amazon.co.uk/registry/wishlist/3GOX0MMJTRD3C?tag=benhuson-21" target="_blank">Buy me something</a> |</li>
 	<li><a href="http://twitter.com/benhuson" target="_blank">Follow me on Twitter</a></li>
 </ul>
 
