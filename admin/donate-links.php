135e2f0863282ad774f7c44dab3e5eabeffed8ec
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-13 22:10:07 +0000

    Translated donate links.
    git-svn-id: https://plugins.svn.wordpress.org/wp-geo/trunk@163198 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/donate-links.php
+++ admin/donate-links.php
@@ -13,10 +13,10 @@
 
 
 <ul class="subsubsub donate-links" style="float:none;">
-	<li>Support this plugin: </li>
-	<li><a href="http://www.wpgeo.com/donate" target="_blank">Make a Donation</a> |</li>
-	<li><a href="http://www.amazon.co.uk/registry/wishlist/3GOX0MMJTRD3C?tag=benhuson-21" target="_blank">Buy me something</a> |</li>
-	<li><a href="http://twitter.com/benhuson" target="_blank">Follow me on Twitter</a></li>
+	<li><?php echo __('Support this plugin', 'wpgeo'); ?>: </li>
+	<li><a href="http://www.wpgeo.com/donate" target="_blank"><?php echo __('Make a Donation', 'wpgeo'); ?></a> |</li>
+	<li><a href="http://www.amazon.co.uk/registry/wishlist/3GOX0MMJTRD3C?tag=benhuson-21" target="_blank"><?php echo __('Buy me something', 'wpgeo'); ?></a> |</li>
+	<li><a href="http://twitter.com/benhuson" target="_blank"><?php echo __('Follow me on Twitter', 'wpgeo'); ?></a></li>
 </ul>
 
 
