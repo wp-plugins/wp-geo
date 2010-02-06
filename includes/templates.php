17b79389baace14a4980c27e108c3bd786dafff8
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-06 21:42:09 +0000

    Added wpgeo_map() function to display custom maps based on a WordPress query.
    git-svn-id: https://plugins.svn.wordpress.org/wp-geo/trunk@202695 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates.php
+++ includes/templates.php
@@ -84,4 +84,54 @@ function get_wpgeo_longitude( $post_id = null ) {
 
 
 
+/**
+ * @method  WP Geo Map
+ */
+
+function wpgeo_map( $query, $options = null ) {
+	
+	global $wpgeo_map_id;
+	
+	$wpgeo_map_id++;
+	
+	$id = 'wpgeo_map_id_' . $wpgeo_map_id;
+	
+	$wp_geo_options = get_option('wp_geo_options');
+	
+	$posts = get_posts( $query );
+	
+	echo '
+		<div id="' . $id . '" class="wpgeo_map" style="width:' . $wp_geo_options['default_map_width'] . '; height:' . $wp_geo_options['default_map_height'] . ';"></div>
+		<script type="text/javascript">
+		<!--
+		jQuery(window).load( function() {
+			if ( GBrowserIsCompatible() ) {
+				var bounds = new GLatLngBounds();
+				map = new GMap2(document.getElementById("' . $id . '"));
+				map.addControl(new GLargeMapControl3D());
+				';
+	foreach ( $posts as $post ) {
+		$latitude = get_post_meta($post->ID, WPGEO_LATITUDE_META, true);
+		$longitude = get_post_meta($post->ID, WPGEO_LONGITUDE_META, true);
+		if ( is_numeric($latitude) && is_numeric($longitude) ) {
+			echo '
+				var center = new GLatLng(' . $latitude . ',' . $longitude . ');
+				var marker = new wpgeo_createMarker2(map, center, wpgeo_icon_small, \'' . $post->post_title . '\', \'' . get_permalink($post->ID) . '\');
+				bounds.extend(center);
+				';
+		}
+	}
+	echo '
+				zoom = map.getBoundsZoomLevel(bounds);
+				map.setCenter(bounds.getCenter(), zoom);
+			}
+		} );
+		-->
+		</script>
+		';
+	
+}
+
+
+
 ?>
