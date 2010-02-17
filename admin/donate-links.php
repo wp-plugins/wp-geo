46a5a89daf56568411f755e01585410515508cb6
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-17 21:27:59 +0000

    Updated donate links.
    git-svn-id: https://plugins.svn.wordpress.org/wp-geo/trunk@207318 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/donate-links.php
+++ admin/donate-links.php
@@ -16,7 +16,6 @@
 <ul class="subsubsub donate-links" style="float:none;">
 	<li><?php echo __('Support this plugin', 'wp-geo'); ?>: </li>
 	<li><a href="http://www.wpgeo.com/donate" target="_blank"><?php echo __('Make a Donation', 'wp-geo'); ?></a> |</li>
-	<li><a href="http://www.amazon.co.uk/registry/wishlist/3GOX0MMJTRD3C?tag=benhuson-21" target="_blank"><?php echo __('Buy me something', 'wp-geo'); ?></a> |</li>
 	<li><a href="http://twitter.com/benhuson" target="_blank"><?php echo __('Follow me on Twitter', 'wp-geo'); ?></a></li>
 </ul>
 
