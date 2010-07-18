4bfb0e89e19898efba908c1ada446dc1d9a7d2bc
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-18 11:12:53 +0000

    Tag 3.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-geo/tags/3.2@264352 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates.php
@@ -0,0 +1,250 @@
+<?php
+
+
+
+/**
+ * @package    WP Geo
+ * @subpackage Includes > Template
+ * @author     Ben Huson <ben@thewhiteroom.net>
+ */
+
+
+
+/**
+ * @method       WP Geo Latitude
+ * @description  Outputs the post latitude.
+ * @param        $post_id = Post ID (optional)
+ */
+
+function wpgeo_latitude( $post_id = null ) {
+
+	echo get_wpgeo_latitude( $post_id );
+	
+}
+
+
+
+/**
+ * @method       WP Geo Longitude
+ * @description  Outputs the post longitude.
+ * @param        $post_id = Post ID (optional)
+ */
+
+function wpgeo_longitude( $post_id = null ) {
+
+	echo get_wpgeo_longitude( $post_id );
+	
+}
+
+
+
+/**
+ * @method       WP Geo Title
+ * @description  Outputs the post title.
+ * @param        $post_id = Post ID (optional)
+ * @param        $default_to_post_title = Default to post title if point title empty (optional)
+ */
+
+function wpgeo_title( $post_id = null, $default_to_post_title = true ) {
+
+	echo get_wpgeo_title( $post_id, $default_to_post_title );
+	
+}
+
+
+
+/**
+ * @method       Get WP Geo Latitude
+ * @description  Gets the post latitude.
+ * @param        $post_id = Post ID (optional)
+ * @return       (float) Latitude
+ */
+
+function get_wpgeo_latitude( $post_id = null ) {
+
+	global $post;
+	
+	$id = absint($post_id) > 0 ? absint($post_id) : $post->ID;
+	
+	if ( absint($id) > 0 ) {
+		return get_post_meta( absint($id), WPGEO_LATITUDE_META, true );
+	}
+	
+	return null;
+	
+}
+
+
+
+/**
+ * @method       Get WP Geo Longitude
+ * @description  Gets the post longitude.
+ * @param        $post_id = Post ID (optional)
+ * @return       (float) Longitude
+ */
+
+function get_wpgeo_longitude( $post_id = null ) {
+	
+	global $post;
+	
+	$id = absint($post_id) > 0 ? absint($post_id) : $post->ID;
+	
+	if ( absint($id) > 0 ) {
+		return get_post_meta( absint($id), WPGEO_LONGITUDE_META, true );
+	}
+	
+	return null;
+	
+}
+
+
+
+/**
+ * @method       Get WP Geo Title
+ * @description  Gets the post title.
+ * @param        $post_id = Post ID (optional)
+ * @param        $default_to_post_title = Default to post title if point title empty (optional)
+ * @return       (string) Title
+ */
+
+function get_wpgeo_title( $post_id = null, $default_to_post_title = true ) {
+	
+	global $post;
+	
+	$id = absint( $post_id ) > 0 ? absint( $post_id ) : $post->ID;
+	
+	if ( absint( $id ) > 0 ) {
+		$title = get_post_meta( $id, WPGEO_TITLE_META, true );
+		if ( empty( $title ) && $default_to_post_title ) {
+			$title = $post->post_title;
+		}
+		$title = apply_filters( 'wpgeo_point_title', $title, $id );
+		return $title;
+	}
+	
+	return null;
+	
+}
+
+
+
+/**
+ * @method       WP Geo Post Map
+ * @description  Outputs the HTML for a post map.
+ * @param        $post_id = Post ID (optional)
+ */
+
+function wpgeo_post_map( $post_id = null ) {
+
+	echo get_wpgeo_post_map( $post_id );
+	
+}
+
+
+
+/**
+ * @method       Get WP Geo Post Map
+ * @description  Gets the HTML for a post map.
+ * @param        $post_id = Post ID (optional)
+ * @return       (string) HTML
+ */
+
+function get_wpgeo_post_map( $post_id = null ) {
+
+	global $post, $wpgeo;
+	
+	$id = absint($post_id) > 0 ? absint($post_id) : $post->ID;
+	$wp_geo_options = get_option( 'wp_geo_options' );
+	
+	$show_post_map = apply_filters( 'wpgeo_show_post_map', $wp_geo_options['show_post_map'], $id );
+	
+	if ( $id > 0 && !is_feed() ) {
+		if ( $wpgeo->show_maps() && $show_post_map != 'TOP' && $show_post_map != 'BOTTOM' && $wpgeo->checkGoogleAPIKey() ) {
+			
+			$map_width = $wp_geo_options['default_map_width'];
+			$map_height = $wp_geo_options['default_map_height'];
+		
+			if ( is_numeric( $map_width ) ) {
+				$map_width = $map_width . 'px';
+			}
+		
+			if ( is_numeric( $map_height ) ) {
+				$map_height = $map_height . 'px';
+			}
+			
+			return '<div class="wp_geo_map" id="wp_geo_map_' . $id . '" style="width:' . $map_width . '; height:' . $map_height . ';"></div>';
+		}
+	}
+	
+	return '';
+	
+}
+
+
+
+/**
+ * @method  Get WP Geo Map
+ */
+
+function get_wpgeo_map( $query, $options = null ) {
+	
+	global $wpgeo_map_id;
+	
+	$wpgeo_map_id++;
+	
+	$id = 'wpgeo_map_id_' . $wpgeo_map_id;
+	
+	$wp_geo_options = get_option('wp_geo_options');
+	
+	$posts = get_posts( $query );
+	
+	$output = '
+		<div id="' . $id . '" class="wpgeo_map" style="width:' . $wp_geo_options['default_map_width'] . '; height:' . $wp_geo_options['default_map_height'] . ';"></div>
+		<script type="text/javascript">
+		<!--
+		jQuery(window).load( function() {
+			if ( GBrowserIsCompatible() ) {
+				var bounds = new GLatLngBounds();
+				map = new GMap2(document.getElementById("' . $id . '"));
+				map.addControl(new GLargeMapControl3D());
+				';
+	foreach ( $posts as $post ) {
+		$latitude = get_post_meta($post->ID, WPGEO_LATITUDE_META, true);
+		$longitude = get_post_meta($post->ID, WPGEO_LONGITUDE_META, true);
+		if ( is_numeric($latitude) && is_numeric($longitude) ) {
+			$icon = apply_filters( 'wpgeo_marker_icon', 'wpgeo_icon_small', $post, 'wpgeo_map' );
+			$output .= '
+				var center = new GLatLng(' . $latitude . ',' . $longitude . ');
+				var marker = new wpgeo_createMarker2(map, center, ' . $icon . ', \'' . $post->post_title . '\', \'' . get_permalink($post->ID) . '\');
+				bounds.extend(center);
+				';
+		}
+	}
+	$output .= '
+				zoom = map.getBoundsZoomLevel(bounds);
+				map.setCenter(bounds.getCenter(), zoom);
+			}
+		} );
+		-->
+		</script>
+		';
+	
+	return $output;
+	
+}
+
+
+
+/**
+ * @method  WP Geo Map
+ */
+
+function wpgeo_map( $query, $options = null ) {
+
+	echo get_wpgeo_map( $query, $options );
+	
+}
+
+
+
+?>
---
+++ wp-geo.php
@@ -0,0 +1,98 @@
+<?php
+
+
+
+/*
+Plugin Name: WP Geo
+Plugin URI: http://www.wpgeo.com/
+Description: Adds geocoding to WordPress.
+Version: 3.1.4
+Author: Ben Huson
+Author URI: http://www.wpgeo.com/
+Minimum WordPress Version Required: 2.5
+Tested up to: 2.9.2
+*/
+
+
+
+// Pre-2.6 compatibility
+if ( !defined( 'WP_CONTENT_URL' ) )
+	define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
+if ( !defined( 'WP_CONTENT_DIR' ) )
+	define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+if ( !defined( 'WP_PLUGIN_URL' ) )
+	define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
+if ( !defined( 'WP_PLUGIN_DIR' ) )
+	define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
+
+
+
+// Constants
+define( 'WPGEO_LATITUDE_META',     '_wp_geo_latitude' );
+define( 'WPGEO_LONGITUDE_META',    '_wp_geo_longitude' );
+define( 'WPGEO_TITLE_META',        '_wp_geo_title' );
+define( 'WPGEO_MAP_SETTINGS_META', '_wp_geo_map_settings' );
+
+
+
+// Language
+load_plugin_textdomain( 'wp-geo', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+
+
+
+// Includes
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/wp-geo.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/query.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/marker.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/markers.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/maps.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/functions.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/templates.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/shortcodes.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/feeds.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/display.php' );
+include_once( WP_PLUGIN_DIR . '/wp-geo/includes/widgets.php' );
+
+
+
+// Admin Includes
+if ( is_admin() ) {
+	include_once( WP_PLUGIN_DIR . '/wp-geo/admin/editor.php' );
+	include_once( WP_PLUGIN_DIR . '/wp-geo/admin/dashboard.php' );
+}
+
+
+
+// Init.
+global $wpgeo;
+$wpgeo = new WPGeo();
+
+
+
+// Activation Hook
+register_activation_hook( __FILE__, array( $wpgeo, 'register_activation' ) );
+
+
+
+// Action Hooks
+add_action( 'init', array( $wpgeo, 'init' ) );
+add_action( 'init', array( $wpgeo, 'init_later' ), 10000 );
+add_action( 'wp_print_scripts', array( $wpgeo, 'includeGoogleMapsJavaScriptAPI' ) );
+add_action( 'wp_head', array( $wpgeo, 'wp_head' ) );
+add_action( 'wp_footer', array( $wpgeo, 'wp_footer' ) );
+add_action( 'admin_init', array( $wpgeo, 'admin_init' ) );
+add_action( 'admin_head', array( $wpgeo, 'admin_head' ) );
+add_action( 'admin_menu', array( $wpgeo, 'admin_menu' ) );
+add_action( 'after_plugin_row', array( $wpgeo, 'after_plugin_row' ) );
+
+
+
+// Filters
+add_filter( 'the_content', array( $wpgeo, 'the_content' ) );
+add_filter( 'post_limits', array( $wpgeo, 'post_limits' ) );
+add_filter( 'posts_join', array( $wpgeo, 'posts_join' ) );
+add_filter( 'posts_where', array( $wpgeo, 'posts_where' ) );
+
+
+
+?>
